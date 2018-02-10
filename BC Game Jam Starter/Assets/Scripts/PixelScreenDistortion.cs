2e444b49ad0383ae9ddf00e6dfe6022468c9e60a
Daniel Savage <dansvg@gmail.com>
2018-02-09 22:40:48 -0800

    Add Pixel distortions

---
+++ BC Game Jam Starter/Assets/Scripts/PixelScreenDistortion.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelScreenDistortion : MonoBehaviour {
+
+    /// <summary>
+    /// The visible area to show.
+    /// </summary>
+    public float VisibleAreaRadius = 200;
+
+    /// <summary>
+    /// How big should the "distorted" pixels be?
+    /// </summary>
+    public float PixelGranularity = 16;
+
+    /// <summary>
+    /// The material used to distort the screen.
+    /// </summary>
+    private UnityEngine.Material pixelMaterial;
+
+    private void Awake()
+    {
+        this.pixelMaterial = new Material(Shader.Find("Hidden/PixelDistortion"));
+    }
+
+    /// <summary>
+    /// Called each time we render to the screen.
+    /// </summary>
+    /// <param name="inputTexture">The source texture to use when applying the distortion.</param>
+    /// <param name="outputTexture">The texture we want to write to with our finished product.</param>
+	public void OnRenderImage(RenderTexture inputTexture, RenderTexture outputTexture)
+    {
+        this.pixelMaterial.SetFloat("_pixelGranularity", this.PixelGranularity);
+        this.pixelMaterial.SetFloat("_radius", this.VisibleAreaRadius);
+        this.pixelMaterial.SetFloat("_cursorX", 100 + 200 * Mathf.Sin(UnityEngine.Time.time));
+        this.pixelMaterial.SetFloat("_cursorY", 200.0f);
+        this.pixelMaterial.SetFloat("_textureWidth", inputTexture.width);
+        this.pixelMaterial.SetFloat("_textureHeight", inputTexture.height);
+
+        Graphics.Blit(inputTexture, outputTexture, this.pixelMaterial);
+    }
+}
---
+++ BC Game Jam Starter/Assets/Shaders/PixelDistortion.shader
@@ -0,0 +1,60 @@
+﻿Shader "Hidden/PixelDistortion"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+		SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			uniform float _pixelGranularity;
+			uniform float _radius;
+			uniform float _cursorX;
+			uniform float _cursorY;
+			uniform float _textureWidth;
+			uniform float _textureHeight;
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			sampler2D _MainTex;
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 col = tex2D(_MainTex, i.uv);
+				if (sqrt(pow((i.vertex.xy.x - _cursorX), 2.0) + pow((i.vertex.xy.y - _cursorY),2.0)) > _radius) {
+					col = tex2D(_MainTex, float2(_pixelGranularity * floor(i.vertex.xy.x / _pixelGranularity) / _textureWidth, (1 - (_pixelGranularity * floor(i.vertex.xy.y / _pixelGranularity)) / _textureHeight)));
+				}
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
