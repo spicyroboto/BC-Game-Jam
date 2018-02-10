eebcf32eb09ff0d1afb15bcd9b94b71a8798c8fb
Natasha Miner <nminer@live.ca>
2018-02-10 12:57:37 -0800

    Game over screen

--- BC Game Jam Starter/Assets/Images/Movement.cs
+++ BC Game Jam Starter/Assets/Images/Movement.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
     public float speed;
+    public Text countText;
 
     private Rigidbody2D rb2d;
+    private int count;
 
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        SetCountText();
     }
 
     // Update is called once per frame
@@ -27,5 +32,20 @@ public class Movement : MonoBehaviour {
 		
         rb2d.velocity = inputVector * speed;
 	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag ("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+    }
 
 }
---
+++ BC Game Jam Starter/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public float timeRemaining = 5;
+    public Text timerText;
+    public string levelToLoad;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        SubtractTimeLeft(Time.deltaTime);
+        UpdateText(Mathf.Floor(timeRemaining).ToString());
+        GameOver();
+    }
+
+    /// <summary>
+    /// Reduce <see cref="timeRemaining"/> by a specified amount.
+    /// </summary>
+    /// <param name="delta">The amount to subtract the time by.</param>
+    private void SubtractTimeLeft(float delta)
+    {
+        timeRemaining -= delta;
+    }
+
+    private void GameOver()
+    {
+        if (timeRemaining <= 0)
+            SceneManager.LoadScene(levelToLoad);
+    }
+    
+    
+
+    private void UpdateText(string textToUpdateWith)
+    {
+        timerText.text = textToUpdateWith;
+    }
+}
